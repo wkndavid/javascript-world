410bad9f99ec7d57d1434d56f678e508d961ddfe
wkndavid <workingnown@gmail.com>
2023-06-30 15:11:00 -0300

    Async/Await Examples CDF

--- async-await/scripts.js
+++ async-await/scripts.js
@@ -8,7 +8,7 @@ function bestRockBand(band){
         } else {
             reject({
                 sucess: false,
-                msg: 'I\'m not sure!'
+                msg: 'I´m not sure!'
             })
         }
     })
@@ -16,9 +16,33 @@ function bestRockBand(band){
 function bestRockSong(response) {
     return new Promise((resolve, reject) => {
         if (response.sucess) {
-            resolve('Bohemian Rhapsody by' + response.bandName)
+            resolve('Bohemian Rhapsody by ' + response.bandName)
         } else {
             reject('Do you know Queen?')
         }
     })
 }
+/*
+bestRockBand('restart')
+    .then(response => {
+        console.log('Checking the answer...')
+        return bestRockSong(response)
+    })
+    .then(response => {
+        console.log('Find the best song...')
+        console.log(response)
+    })
+    .catch(err.msg){
+        console.log(msg)
+    }
+*/
+async function doTheJob(){
+   try {
+     const bestRockBandResponse = await bestRockBand('Queen');
+    const bestRockSongResponse = await bestRockSong(bestRockBandResponse);
+    console.log(bestRockSongResponse);
+    } catch(err){
+        console.log(err.msg)
+    }
+}
+doTheJob();
